33091093bc66323c877d67717155004df11c045c
Praveen Rajagopalan <praveen.rajagopalan@coreos.com>
2017-09-18 16:31:04 -0400

    Change to app.py with new version

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def index():
 
     timestamp2 = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     app.logger.debug("Finished at: " + timestamp2)
-    return timestamp2 + " Hello, World! - v2 Updated\n"
+    return timestamp2 + " Hello, World! - v3 Updated\n"
 
 if __name__ == '__main__':
     monitor(app, port=8000)
